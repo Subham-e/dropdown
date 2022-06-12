e6434e08deebc3a2a01743acbce0fc454c4468b2
Subham Rai <93194290+Subham-e@users.noreply.github.com>
2022-06-12 21:19:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var features =   document.querySelector(".features div");
+var company =   document.querySelector(".company div");
+
+document.querySelector(".features button").addEventListener("click", function(){
+   features.classList.toggle("show");
+   company.classList.remove("show");
+});
+
+ document.querySelector(".company button").addEventListener("click", function(){
+  company.classList.toggle("show");
+  features.classList.remove("show");
+});
+
+document.querySelector(".togglebtn").addEventListener("click",function(){
+
+    if( document.querySelector(".menu").getAttribute("src") == "images/icon-menu.svg"){
+     document.querySelector(".menu").src = "images/icon-close-menu.svg";
+     document.querySelector(".navsection").style.display = "flex";
+
+   }else if( document.querySelector(".menu").getAttribute("src") == "images/icon-close-menu.svg"){
+   document.querySelector(".menu").src = "images/icon-menu.svg";
+   document.querySelector(".navsection").style.display = "none";
+
+
+   }
+});
